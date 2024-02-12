d1719f817d968b9be5406d8e2ca488420332bff2
radu <radu.grosu@gmail.com>
2024-02-12 18:23:59 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
+
+mod serial;
+mod vga_buffer;
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World this is a very long message that shoulnd't wrap around and should produce a failure of some sort{}", "!");
+    // panic!("Some panic message");
+
+    blog_os::init();
+
+    unsafe {
+        *(0xdeadbeef as *mut u8) = 42;
+    };
+
+    // x86_64::instructions::interrupts::int3();
+
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
+    loop {}
+}
